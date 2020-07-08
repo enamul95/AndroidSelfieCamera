3dd0dfda0e436f736634712ce89c661f9c1e8dab
Soyeb189 <muktadir189@gmai.com>
2020-07-08 09:32:40 +0600

    Camera added

--- app/src/main/java/com/era/androidselfiecamera/MainActivity.kt
+++ app/src/main/java/com/era/androidselfiecamera/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.era.androidselfiecamera
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnSelfie:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSelfie = findViewById(R.id.btnSelfie);
+        button.isEnabled = false
 
-        btnSelfie.setOnClickListener {
-            Toast.makeText(applicationContext,"Selfie Camera",Toast.LENGTH_LONG).show()
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA),111)
+        }else{
+            button.isEnabled = true
+        }
+
+        button.setOnClickListener {
+            var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            i.putExtra("android.intent.extras.LENS_FACING_FRONT", 1);
+            i.putExtra("android.intent.extra.USE_FRONT_CAMERA", true);
+            startActivityForResult(i,101)
+
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            button.isEnabled = true;
         }
     }
 }
